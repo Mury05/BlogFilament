8f464329aa51840dcaf45e3dd2f31a113eacef70
Frejus DASSI <frejohope@gmail.com>
2025-01-28 17:13:31 +0100

    Ajout des colonnes pour afficher le contenu, l'article, l'auteur et la date de création dans la liste des commentaires

--- app/Filament/Resources/CommentResource/Pages/ListComments.php
+++ app/Filament/Resources/CommentResource/Pages/ListComments.php
@@ -5,6 +5,9 @@ namespace App\Filament\Resources\CommentResource\Pages;
 use App\Filament\Resources\CommentResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
 
 class ListComments extends ListRecords
 {
@@ -14,6 +17,16 @@ class ListComments extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    protected function getTableColumns(): array
+    {
+        return [
+            TextColumn::make('content')->label('Contenu'),
+            TextColumn::make('post.title')->label('Article'),
+            TextColumn::make('user.name')->label('Auteur')->sortable(),
+            TextColumn::make('created_at')->label('Créé le')->dateTime(),
         ];
     }
 }
