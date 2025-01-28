984fc5bbb9548c63e3c2d51a58cbc689dfc50454
Frejus DASSI <frejohope@gmail.com>
2025-01-28 17:13:03 +0100

    Ajout du formulaire de création de commentaire avec les champs contenu, article et auteur

--- app/Filament/Resources/CommentResource/Pages/CreateComment.php
+++ app/Filament/Resources/CommentResource/Pages/CreateComment.php
@@ -4,9 +4,31 @@ namespace App\Filament\Resources\CommentResource\Pages;
 
 use App\Filament\Resources\CommentResource;
 use Filament\Actions;
+use Filament\Forms\Components\BelongsToSelect;
+use Filament\Forms\Components\Textarea;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateComment extends CreateRecord
 {
     protected static string $resource = CommentResource::class;
+
+    protected function getFormSchema(): array
+    {
+        return [
+            Textarea::make('content')
+                ->required()
+                ->maxLength(500)
+                ->label('Contenu'),
+            
+            BelongsToSelect::make('post_id')
+                ->relationship('post', 'title')
+                ->required()
+                ->label('Article'),
+            
+            BelongsToSelect::make('user_id')
+                ->relationship('user', 'name')
+                ->nullable()
+                ->label('Auteur'),
+        ];
+    }
 }
