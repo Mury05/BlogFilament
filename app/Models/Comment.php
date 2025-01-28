48c1172b5bb919886d34a3efa8dc1378723fa018
Frejus DASSI <frejohope@gmail.com>
2025-01-28 15:11:56 +0100

    Ajout des relations entre Comment, Post et User dans le modèle Comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    //relation entre post et comment
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+
+    //relation entre user et comment
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
